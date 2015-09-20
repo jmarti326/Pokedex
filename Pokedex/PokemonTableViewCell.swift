ae78884f90cb831b7060445086af3c681fcce42d
Juan R. Martí Mercado <jmarti.dev@gmail.com>
2015-09-20 14:59:21 -0400

    Did some small modifications to the cell.  Created the AllPokemonTableViewController and the PokemonTableViewCell.  Connected all the objects from the row to the PokemonTableViewCell.

---
+++ Pokedex/PokemonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonTableViewCell.swift
+//  Pokedex
+//
+//  Created by Juan Martí on 9/20/15.
+//  Copyright © 2015 Juan Martí. All rights reserved.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
